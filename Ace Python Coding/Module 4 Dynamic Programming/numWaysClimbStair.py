7cb8fd126ea90baccce70e2a6a5acd9951987e11
Krupesh Anadkat <kmanadkat@gmail.com>
2021-09-17 00:07:05 +0530

    Solved Num Ways DP Problem

---
+++ Ace Python Coding/Module 4 Dynamic Programming/numWaysClimbStair.py
@@ -0,0 +1,69 @@
+
+# Time : O(n)
+# Space: O(1)
+# Should be 2nd approach - optimizationn
+def numWays2(n):
+    # Steps 1,2,4
+    ways = [0, 1, 2, 3, 6]
+
+    if n < 0:
+        return 0
+
+    if n <= 4:
+        return ways[n]
+
+    for i in range(5, n+1):
+        numWaysForI = ways[1] + ways[2] + ways[4]
+        ways[0] = ways[1]
+        ways[1] = ways[2]
+        ways[2] = ways[3]
+        ways[4] = numWaysForI
+
+    return ways[n]
+
+
+# Time & Space : O(n)
+def numWays(n):
+    # Steps 1,2,4
+    ways = [0, 1, 2, 3, 6]
+
+    if n < 0:
+        return 0
+
+    if n <= 4:
+        return ways[n]
+
+    for i in range(5, n+1):
+        # Recurrence Relation - s(n) = s(n-1) + s(n-2) + s(n-4)
+        numWaysForI = ways[i - 1] + ways[i-2] + ways[i - 4]
+        ways.append(numWaysForI)
+
+    return ways[n]
+
+
+print(numWays(5))
+print(numWays(6))
+print(numWays(7))
+print(numWays(8))
+
+print("Edge Cases")
+print(numWays(0))
+print(numWays(1))
+print(numWays(2))
+print(numWays(3))
+print(numWays(4))
+print(numWays(-1))
+
+print("Efficient Soln")
+print(numWays(5))
+print(numWays(6))
+print(numWays(7))
+print(numWays(8))
+
+print("Edge Cases")
+print(numWays(0))
+print(numWays(1))
+print(numWays(2))
+print(numWays(3))
+print(numWays(4))
+print(numWays(-1))
